f4b1bbdc24367cdc0b0282d5c7ad3fe546d8e2a3
Kevin Peters <kwpeters@ra.rockwell.com>
2021-11-06 17:18:13 -0400

    Removed hr.ps1.  Now have it in clitools.

--- hr.ps1
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env pwsh
-
-$sep = "--------------------------------------------------------------------------------"
-Write-Output ""
-Write-Output ""
-Write-Output $sep
-Write-Output ""
-Write-Output ""
