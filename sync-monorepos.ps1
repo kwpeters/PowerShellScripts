beab8c56890586d4d310e3c0ca92abe9174c1f17
Kevin Peters <kwpeters@gmail.com>
2023-04-19 07:31:45 -0400

    Added script to sync monorepos.

---
+++ sync-monorepos.ps1
@@ -0,0 +1,3 @@
+#!/usr/bin/env pwsh
+
+ideadiff.cmd C:\Users\kwpeters\dev\kwp\monorail\packages\depot\ C:\Users\kwpeters\dev\rok\lgx\rokmono\packages\depot\ && ideadiff.cmd C:\Users\kwpeters\dev\kwp\monorail\packages\depot-node\ C:\Users\kwpeters\dev\rok\lgx\rokmono\packages\depot-node\
